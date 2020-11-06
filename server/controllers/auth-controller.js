ccae7ef9192d82f25d18a24548c2fb6573987113
Mayya Markova <mayya.markova@gmail.com>
2020-11-06 14:51:38 +0200

    #14 Add log in functionality

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -2,6 +2,7 @@ import express from 'express';
 import createToken from '../auth/create-token.js';
 import bodyValidator from '../middlewares/body-validator.js';
 import userRegistrationSchema from '../validators/user-registration.schema.js';
+import userLogInSchema from '../validators/user-login.schema.js';
 import { USER_ROLES } from '../config.js';
 import usersService from '../services/users-service.js';
 import serviceErrors from '../services/errors-service.js';
@@ -28,6 +29,31 @@ authController.post('/registration', bodyValidator(userRegistrationSchema), asyn
   
   const token = createToken(payload);
   
+  res.status(200).send({ token });
+});
+
+authController.post('/session', bodyValidator(userLogInSchema), async (req, res) => {
+  const logInData = req.body;
+  
+  const { user, userError } = await usersService.getLoggedUser(usersData)(logInData);
+
+  if (userError === serviceErrors.RESOURCE_NOT_FOUND) {
+    return res.status(404).send({ message: 'User is not found!' });
+  }  
+  if (userError === serviceErrors.BAD_REQUEST) {
+    return res.status(401).send({ message: 'Invalid credentials!' });
+  } 
+  
+  const payload = {
+    sub: user.id,
+    username: user.username,
+    firstName: user.firstName,
+    lastName: user.lastName,
+    role: user.role === 'teacher' ? USER_ROLES.TEACHER : USER_ROLES.STUDENT,
+  };
+
+  const token = createToken(payload);
+
   res.status(200).send({ token });
 });
 
--- server/services/users-service.js
+++ server/services/users-service.js
@@ -20,6 +20,33 @@ const registerUser = (usersData) => {
   };
 };
 
+const getLoggedUser = (usersData) => {
+  return async (logInData) => {
+    const { username, password } =  logInData;
+
+    const user = await usersData.getByUsername(username);
+
+    if (!user) {
+      return {
+        user: null,
+        userError: serviceErrors.RESOURCE_NOT_FOUND,
+      };
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordCorrect) {
+      return {
+        user: null,
+        userError: serviceErrors.BAD_REQUEST,
+      };
+    }
+    
+    return { user, userError: null };
+
+  };
+};
 export default {
   registerUser,
+  getLoggedUser,
 };
