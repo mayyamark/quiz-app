15ad1b25b4e68afbce9dfc053453b309d9a5f00f
Mayya Markova <mayya.markova@gmail.com>
2020-11-06 15:36:01 +0200

    #15 Add middleware for checking token

---
+++ server/middlewares/check-token-middlware.js
@@ -0,0 +1,19 @@
+import serviceErrors from '../services/errors-service.js';
+import blacklistData from '../data/blacklist-data/blacklist-data.js';
+
+const checkTokenMiddleware = (usersService) => async (req, res, next) => {
+  const token =  req.headers.authorization.split(' ')[1];
+
+  if (!token) {
+    return res.status(404).json({ message: 'No token in the request!' });
+  }
+
+  const { tokenError } = await usersService.isUserLoggedOut(blacklistData)(token);
+  if (tokenError === serviceErrors.UNAUTHORIZED) {
+    return res.status(403).json({ message: 'User is not logged in!' });
+  }
+
+  next();
+};
+
+export default checkTokenMiddleware;
