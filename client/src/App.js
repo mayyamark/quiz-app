2a34876cb154d0cb3c2a48f6fd39ec18260bc7ce
Mayya Markova <mayya.markova@gmail.com>
2020-11-12 09:50:13 +0200

    #16 Fix bug when user is logged in & refreshes

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,33 @@
-import AuthProvider from './auth/AuthProvider';
-import { useAuth } from './auth/AuthContext';
+import { useState, useEffect } from 'react';
+import decode from 'jwt-decode';
+import { AuthContext } from './auth/AuthContext.js';
+import { getToken, removeToken } from './common/manage-token.js';
 import PublicApp from './components/public/PublicApp';
+import PrivateApp from './components/private/PrivateApp';
 import './App.css';
 
 const App = () => {
-  const { user } = useAuth();
+  const token = getToken();
 
-  return (
-    <AuthProvider>
-      {user ? <PrivateApp /> : <PublicApp />}
-    </AuthProvider>
+  const [user, setUser] = useState(token ?
+    decode(token)
+    : null);
+
+    useEffect(() => {
+      if (user) {
+        setTimeout(() => {
+          removeToken();
+          setUser(null);
+        }, user.exp * 1000 - Date.now());
+      }
+      return () => clearTimeout();
+    }, [user]);
+
+
+    return (
+      <AuthContext.Provider value={{ user, setUser }}>
+        {user ? <PrivateApp /> : <PublicApp />}
+      </AuthContext.Provider>
   );
 };
 
--- client/src/components/private/PrivateApp.js
+++ client/src/components/private/PrivateApp.js
@@ -1,22 +1,28 @@
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect, useHistory } from 'react-router-dom';
 import { useAuth } from '../../auth/AuthContext';
-import Login from '../../containers/public/Authentication/Login';
 import StudentDashboard from './StudentDashboard/StudentDashboard';
+import Leaderboard from '../../containers/private/Leaderboard/Leaderboard';
+import { useEffect } from 'react';
+import StudentRoute from '../../auth/StudentRoute';
 
 const PrivateApp = () => {
   const { user } = useAuth();
+  const history = useHistory();
 
-  if (!user) {
-    return <Login />;
-  } 
+  useEffect(() => {
+    if (!user) {
+      history.push('/login');
+    }
+  }, [history, user]);
 
   return (
   <BrowserRouter>
     <Switch>
-    <Route path="/dashboard" component={StudentDashboard} />
-      <Route path="*">
-        <Redirect to="/dashboard" />
-      </Route>
+    <StudentRoute path="/dashboard" component={StudentDashboard} />
+    <StudentRoute path="/leaderboard" component={Leaderboard} />
+    <Route path="*">
+      <Redirect to="/dashboard" />
+    </Route>
     </Switch>
   </BrowserRouter>);
 };
