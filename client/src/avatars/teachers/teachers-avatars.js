1cf5d8b123e4e7e92b0b42c4b49ec8b91b559644
Mayya Markova <mayya.markova@gmail.com>
2020-11-19 08:08:52 +0200

    #1 Add teachers' avatars & add users' data to DB

---
+++ client/src/avatars/teachers/teachers-avatars.js
@@ -0,0 +1,24 @@
+import a1 from './1.png';
+import a2 from './2.png';
+import a3 from './3.png';
+import a4 from './4.png';
+import a5 from './5.png';
+import a6 from './6.jpg';
+import a7 from './7.png';
+import a8 from './8.jpg';
+import a9 from './9.jpg';
+import a10 from './10.jpg';
+import a11 from './11.jpg';
+import a12 from './12.jpg';
+import a13 from './13.jpg';
+import a14 from './14.jpg';
+import a15 from './15.jpg';
+import a16 from './16.jpg';
+import a17 from './17.jpg';
+import a18 from './18.jpg';
+import a19 from './19.jpg';
+import a20 from './20.png';
+
+const teachersAvatars = [a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17, a18, a19, a20];
+
+export default teachersAvatars;
