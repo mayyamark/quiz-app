e7e668b7580039d38e3e39fed1a92b65844ff169
Mayya Markova <mayya.markova@gmail.com>
2020-11-19 10:30:06 +0200

    #16 Add style

--- client/src/components/public/LandingPage/LandingPage.js
+++ client/src/components/public/LandingPage/LandingPage.js
@@ -1,12 +1,20 @@
 import { Link } from 'react-router-dom';
+import './LandingPage.css';
 
 const LandingPage = () => {
   return (
-    <div>
-      <Link to="/login">Login</Link>
-      &nbsp;&nbsp;
-      <Link to="/register">Register</Link>
-  </div>
+    <header className="header">
+    <div className="text-box">
+      <h1 className="heading-primary">
+        <span className="heading-primary-main">Dive in!</span>
+        <span className="heading-primary-sub">Check out our quizzes!</span>
+      </h1>
+      <div className="auth-btns">
+        <Link to="/register" className="btn btn-white btn-animated">SIGN UP</Link>
+        <Link to="/login" className="btn btn-white btn-animated">LOG IN</Link>
+      </div>
+    </div>
+  </header>
   );
 };
 
