ba91bba347a9a990a412c8081c43aa521f9cbdeb
Nadya Hrisimova <nadya.hrisimova.a24@learn.telerikacademy.com>
2020-11-21 18:38:59 +0200

    view history uniqie ids in list items

--- client/src/components/private/ViewQuiz/ViewQuizHistoryItem.js
+++ client/src/components/private/ViewQuiz/ViewQuizHistoryItem.js
@@ -3,7 +3,7 @@ import { TextField, ListItem } from '@material-ui/core';
 const ViewQuizHistoryItem = (props) => {
   const { historyItem } = props;
   return (
-    <ListItem>
+    <ListItem  key={historyItem.username}>
       <TextField label="Studen name" InputProps={{readOnly: true}} value={`${historyItem.firstName} ${historyItem.lastName}`}/>
       <TextField label="Points" InputProps={{readOnly: true}} value={`${historyItem.score}`} type="number"/>
       <TextField label="Time taken" InputProps={{readOnly: true}} value={`${historyItem.started.substring(0, historyItem.started.length - 1)}`} type="datetime-local"/>
