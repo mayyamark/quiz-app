a40fc0c255c67a4cc27c0e79ae790fd26c88e4e6
Mayya Markova <mayya.markova@gmail.com>
2020-11-23 15:00:59 +0200

    Fix bugs when new user is registered

--- client/src/components/private/Categories/Categories.js
+++ client/src/components/private/Categories/Categories.js
@@ -2,10 +2,9 @@ import { memo, useEffect } from 'react';
 import CustomTable from '../../common/CustomTable/CustomTable';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { Link } from 'react-router-dom';
-import ErrorPage from '../../common/ErrorPage/ErrorPage';
 import './Categories.css';
 const Categories = memo((props) => {
-  const { categories, loading, error, onInitCategories, hasCategories } = props;
+  const { categories, loading, onInitCategories, hasCategories } = props;
 
   useEffect(() => {
     onInitCategories();
@@ -13,9 +12,7 @@ const Categories = memo((props) => {
 
   return (
     <>
-      {error ? (
-        <ErrorPage />
-      ) : loading ? (
+      {loading ? (
         <CircularProgress />
       ) : hasCategories ? (
         <div id="student-dashboard-categories-container">
