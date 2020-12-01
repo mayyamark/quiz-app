be995286ff24ec7ea403122f0a38772c016e9cbf
Mayya Markova <mayya.markova@gmail.com>
2020-12-01 23:58:57 +0200

    Decorate NavBar and make it responsive

--- client/src/components/common/NavBar/NavBar.js
+++ client/src/components/common/NavBar/NavBar.js
@@ -1,27 +1,101 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { withStyles } from '@material-ui/core/styles';
 import { makeStyles } from '@material-ui/core/styles';
-import Logout from '../../../containers/public/Authentication/Logout';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import Avatar from '@material-ui/core/Avatar';
+import ListItemText from '@material-ui/core/ListItemText';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import Logout from '../../../containers/public/Authentication/Logout';
+import { useAuth } from '../../../auth/AuthContext';
 import './NavBar.css';
+
+const StyledMenu = withStyles({
+  paper: {
+    border: '1px solid #d3d4d5',
+  },
+})((props) => (
+  <Menu
+    elevation={0}
+    getContentAnchorEl={null}
+    anchorOrigin={{
+      vertical: 'bottom',
+      horizontal: 'center',
+    }}
+    transformOrigin={{
+      vertical: 'top',
+      horizontal: 'center',
+    }}
+    {...props}
+  />
+));
+
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
   title: {
-    marginRight: '2%',
+    flexGrow: 1,
   },
 }));
 
 const NavBar = () => {
   const classes = useStyles();
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const { user } = useAuth();
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <div className={classes.root}>
-      <AppBar position="static" style={{ backgroundColor: 'black' }} >
+      <AppBar position="static" style={{ backgroundColor: 'black' }}>
         <Toolbar>
-          <Link id="dashboard-link" to='/dashboard' className={classes.title}>Dashboard</Link>
-          <Logout />
+          <Link id="dashboard-link" to="/dashboard" className={classes.title}>
+            Dashboard
+          </Link>
+          <div>
+            <IconButton
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleMenu}
+              color="inherit"
+            >
+              <ListItemText
+                primary={`${user.firstName.slice(0, 1)}. ${user.lastName}`}
+              />
+              <ArrowDropDownIcon />
+            </IconButton>
+            <StyledMenu
+              id="customized-menu"
+              anchorEl={anchorEl}
+              keepMounted
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              <MenuItem>
+                <Avatar id="user-avatar" src={user.avatar} alt="avatar" />
+                <ListItemText primary={user.username} />
+              </MenuItem>
+              <MenuItem>
+                <Logout />
+              </MenuItem>
+            </StyledMenu>
+          </div>
         </Toolbar>
       </AppBar>
     </div>
